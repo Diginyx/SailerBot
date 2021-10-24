3e1b0c7e5c617e7ba8afbd00a3782eda0f26b701
Josue Torres-Fonseca <50052342+Diginyx@users.noreply.github.com>
2021-10-24 12:22:29 -0600

    Update sailerbot.js

--- sailerbot.js
+++ sailerbot.js
@@ -39,7 +39,7 @@ let twitterChannel = '382670158692614144'; // shitposting_channel
 
 // Create a stream to follow tweets
 const stream = T.stream('statuses/filter', {
-    follow: '1204118236918435843', // @RiseMonday 1204118236918435843 
+    follow: '32771325', // @RiseMonday 1204118236918435843 
   });  
 
 stream.on('tweet', (tweet) => {

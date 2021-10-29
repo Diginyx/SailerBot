3ed1ffb74f3d6e906f667485199459b8736b9983
Josue Torres-Fonseca <josuextorres@gmail.com>
2021-10-29 13:19:44 -0600

    remove slash commands

--- sailerbot.js
+++ sailerbot.js
@@ -193,23 +193,3 @@ client.on('messageReactionRemove', async (messageReaction, user) => {
         }
     } else return;
   });
-
-  client.on('interactionCreate', async interaction => {
-    if (!interaction.isCommand()) return;
-
-    const { commandName } = interaction;
-
-    switch (commandName) {
-        case 'ping':
-            await interaction.reply('Pong!');
-            break;
-        
-        case 'server':
-            await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
-            break;
-
-        case 'user':
-            await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
-            break;
-    }
-});
